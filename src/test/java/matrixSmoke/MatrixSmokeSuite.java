f48df1718b4fc3e5f6e82d3eb2126f1ea0db3b9b
oleksandrivanyshyn <alexastucco@gmail.com>
2019-07-19 22:10:02 -0500

    Update

--- src/test/java/matrixSmoke/MatrixSmokeSuite.java
+++ src/test/java/matrixSmoke/MatrixSmokeSuite.java
@@ -90,4 +90,82 @@ public class MatrixSmokeSuite extends TestBase {
 
         Assert.assertTrue(leavesPage.messageText.isDisplayed(),"Message is not displayed");
     }
+
+    @Test
+    public void clickLeaveButtonRequst()throws Exception{
+        HomePage hp=new HomePage();
+        hp.leavesButton.click();
+
+        LeavesPage lp=new LeavesPage();
+        lp.addSearchFilter.click();
+
+        lp.filterButton.click();
+
+        lp.currentYearFilter.click();
+
+        String actualText ="Current Year";
+        Assert.assertEquals(lp.currentYearFilter.getText(),actualText);
+        Assert.assertTrue(lp.currentYearFilter.isDisplayed());
+
+        @Test
+    public void addDetatilsOfLeaveRequest() {
+        HomePage homePage = new HomePage();
+        homePage.leavesButton.click();
+
+        LeavesPage leaves = new LeavesPage();
+        leaves.createButton.click();
+        leaves.descriptionInput.sendKeys("Vacation");
+        leaves.leaveTypeInput.sendKeys("Unpaid");
+        leaves.daysNumInput.sendKeys("3");
+        leaves.saveButton.click();
+        Assert.assertTrue(leaves.alert.isDisplayed());
+        leaves.okButton.click();
+    }
+    @Test
+    public void locateButtonsInLeaves() {
+        HomePage homePage = new HomePage();
+        homePage.leavesButton.click();
+
+        LeavesPage leaves = new LeavesPage();
+        leaves.leavesSummaryButton.click();
+        String expectedTitle="Leaves Summary";
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle,"Leaves Summary title verification didin't pass");
+        leaves.allocationButton.click();
+        String expectedTitleAllocation="Allocation Request";
+        String actualTitleAllocation=Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitleAllocation,actualTitleAllocation,"Allocation requset title didn't pass");
+        leaves.leaveRequestButton.click();
+        String expectedTitleRequest="Leaves Request";
+        String actualTitleRequest=Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitleRequest,actualTitleRequest,"Leaves request title didn't pass");
+
+
+    }
+        @Test
+        public void LeavesSummaryVerification()throws InterruptedException {
+            HomePage homepage = new HomePage();
+            Thread.sleep(1000);
+            homepage.leavesButton.click();
+            LeavesPage leavesPage = new LeavesPage();
+            leavesPage.leavesSummary.click();
+            Thread.sleep(1500);
+            Assert.assertEquals(leavesPage.leavesSummaryText.getText(), "Leaves Summary");
+        }
+        @Test
+        public void  LeavesSummaryCreateError() throws InterruptedException {
+            HomePage homepage =new HomePage();
+            Thread.sleep(1000);
+            homepage.leavesButton.click();
+            LeavesPage leavesPage = new LeavesPage();
+            leavesPage.leavesSummary.click();
+            Thread.sleep(2000);
+            leavesPage.SummaryCreate.click();
+            Thread.sleep(2000);
+            leavesPage.SummarySave.click();
+            Thread.sleep(1000);
+            Assert.assertTrue(leavesPage.errMessage.isDisplayed());
+
+
+        }
 }
